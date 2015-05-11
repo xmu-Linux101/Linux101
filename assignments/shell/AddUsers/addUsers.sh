273973667dbdd96405ff7de8023c4068a9384d3b
jerry <jerry>
2015-05-11 21:38:25 +0800

    adusers

--- assignments/shell/AddUsers/addUsers.sh
+++ assignments/shell/AddUsers/addUsers.sh
@@ -1,9 +1,8 @@
-#
-#
-# useradd
-# chpasswd
-# chown
-# chmod
-# edquota
-#
+//addusers
+#!/bin/bash
+for name in tom joy john mark james
+do  useradd $name
+    echo "redhat" | passwd --stdin $name
+done
+
 
