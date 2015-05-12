0e40d54dccc63186df40e860efc49827e5359616
Your Name <email@example.com>
2015-05-12 18:00:13 +0800

    1

--- assignments/shell/AddUsers/addUsers.sh
+++ assignments/shell/AddUsers/addUsers.sh
@@ -1,9 +1,15 @@
 #
 #
 # useradd
+cat < users.txt | xargs -n 1 useradd -m -g users
 # chpasswd
+chpasswd < serc.txt
 # chown
+chown users README.md 
 # chmod
+chmod a+w README.md
 # edquota
+edquota -g users 
+edquota -t 
 #
 
