11dc3416708992c7c75f17875f3f335cc789e1b6
WindWood <windwood@gmail.com>
2015-05-13 14:11:20 +0800

    多增加一个隐藏的错误

--- experiments/gcc-5-gdb/array.c
+++ experiments/gcc-5-gdb/array.c
@@ -11,7 +11,7 @@ void array_fill_with(int *array, int length, int fillWith) {
 
 void array_add(int *arrayX, int *arrayY, int *arrayZ, int length) {
 	int i;
-	for (i = 0; i < length; i++) {
+	for (i = 0; i <= length; i++) {
 		arrayZ[i] = arrayX[i] + arrayY[i];
 	}
 }
