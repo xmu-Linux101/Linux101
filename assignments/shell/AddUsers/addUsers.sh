111294f1c08c75b63c56def1260afaaa46316c3f
Your name <you@example.com>
2015-05-13 12:58:07 +0800

    addUsers.sh

--- assignments/shell/AddUsers/addUsers.sh
+++ assignments/shell/AddUsers/addUsers.sh
@@ -1,9 +1,15 @@
+#!/bin/sh
 #
-#
-# useradd
-# chpasswd
-# chown
-# chmod
-# edquota
+for user in `cat /users.txt`
+do  
+	useradd $user
+	echo 123 | passwd --stdin $user
+done
+chpasswd < /userpasswd.txt
+#chown 
+chmod +x ./addUsers.sh
+chmod 755 addUsers.sh
+./addUsers.sh
+edquota -u user
 #
 
