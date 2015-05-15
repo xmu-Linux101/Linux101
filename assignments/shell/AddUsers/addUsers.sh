c8b13ca1095dcaf5d38816c60c64f56be6848f34
angelinshi <angelinshi@qq.com>
2015-05-15 11:22:21 +0800

    linshi-firstline

--- assignments/shell/AddUsers/addUsers.sh
+++ assignments/shell/AddUsers/addUsers.sh
@@ -1,9 +1,10 @@
-#
-#
-# useradd
-# chpasswd
-# chown
-# chmod
-# edquota
-#
-
+#!/bin/bash
+#chmod 700 addUser.sh
+#./addUser.sh
+userlist=`cat users.txt`
+for username in $userlist
+do
+useradd $username
+echo $username:$username"123" | chpasswd
+echo "$username create success!"
+done
