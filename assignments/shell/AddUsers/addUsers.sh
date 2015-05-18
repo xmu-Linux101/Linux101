6179c8229bedf016b2bc8061cc45c18539f32b1d
root <583863755@qq.com>
2015-05-18 18:46:24 +0800

    change

--- assignments/shell/AddUsers/addUsers.sh
+++ assignments/shell/AddUsers/addUsers.sh
@@ -1,9 +1,6 @@
-#
-#
-# useradd
-# chpasswd
-# chown
-# chmod
-# edquota
-#
-
+#!/bin/bash
+for user in `cat /2015512/Linux101/assignments/shell/AddUsers/users.txt`
+do                                                                           sudo useradd $user
+sudo passwd $user
+echo "add ${user} success"
+done
