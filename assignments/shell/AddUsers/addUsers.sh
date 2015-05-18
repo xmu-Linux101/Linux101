fa94f2d3188952fc997b6425d9c94d47ce714b63
weiwei0 <159747491@qq.com>
2015-05-18 19:59:10 +0800

    write the shell to add ursers!

--- assignments/shell/AddUsers/addUsers.sh
+++ assignments/shell/AddUsers/addUsers.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
+for user in `cat /home/adobe/Linux101/assignments/shell/AddUsers/users.txt`
+do
+	useradd $user
+	echo "linux" | passwd -a $user
+	echo "$user create success!"
+done
+chpassed < /home/adobe/Linux101/assignments/shell/AddUsers/passwd
+
+pwconv
 
