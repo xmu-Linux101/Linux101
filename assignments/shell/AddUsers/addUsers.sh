fc794ead02d4ca14d04fda8b1c4518321d856aef
chenjiamin951125 <1198185816@qq.com>
2015-05-20 00:45:06 +0800

    shell脚本

--- assignments/shell/AddUsers/addUsers.sh
+++ assignments/shell/AddUsers/addUsers.sh
@@ -1,9 +1,10 @@
-#
-#
-# useradd
-# chpasswd
-# chown
-# chmod
-# edquota
-#
-
+#!/bin/bash
+#chmod 700 addUsers.sh
+#./addUsers.sh
+for user in `cat /home/shenxia/Linux101/assignments/shell/AddUsers/users`
+do
+useradd -m $user
+echo "$user:redhat" | chpasswd
+echo "$user create success!"
+done
+pwconv
