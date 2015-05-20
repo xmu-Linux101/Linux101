4e1037b4a967504436535c4a66b4efa569b6657c
teamof <1106039881@qq.com>
2015-05-20 16:37:27 +0800

    snjnvdfz

--- assignments/shell/AddUsers/addUsers.sh
+++ assignments/shell/AddUsers/addUsers.sh
@@ -1,6 +1,14 @@
-#
-#
-# useradd
+#!/bin/bash
+for user in `cat /home/linux/learn/Linux101/assignments/shell/AddUsers/users.txt`
+do
+
+     useradd $user
+     echo "linux" | passwd -a $user
+     echo "$user create success!"
+done
+chpasswd < /home/linux/learn/Linux101/assignments/shell/AddUsers/passwd
+
+pwconv
 # chpasswd
 # chown
 # chmod
