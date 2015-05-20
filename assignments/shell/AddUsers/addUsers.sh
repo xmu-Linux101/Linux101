462dcc9ab40cc95f0b8c7f84d5c689837e3e1340
honghuizhen <honghuizhen@honghuizhen-virtual-machine>
2015-05-20 12:36:53 +0800

    shell脚本

--- assignments/shell/AddUsers/addUsers.sh
+++ assignments/shell/AddUsers/addUsers.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-#
-# useradd
-# chpasswd
-# chown
-# chmod
-# edquota
+#chmod 700 addUsers.sh
+#./addUsers.sh
+for user in `cat /home/honghuizhen/Linux101/assignments/shell/AddUsers/users.txt`
+do
+useradd -m $user
+echo "$user:redhat" | chpasswd
+echo "$user create success!"
+done
+pwconv
