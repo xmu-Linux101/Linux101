5b3e9435c7dec0a098e44841ce51316704164ef2
Zhangbing323 <1053861427@qq.com>
2015-06-03 16:39:57 +0800

    Update main.c

--- final-test/c_cpp/main.c
+++ final-test/c_cpp/main.c
@@ -1,22 +1,42 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+ stack s;
+ int i;
+ char *elem="acdef";
+ char *temp;
+ //栈的初始化；
+ initStack(&s);
+ //插入元素a,b,c,d,e调用push函数
+ printf("进栈顺序：\n");
+ temp=elem;
+ while(*elem!='\0')
+ {
+	  printf("%3c",*elem);
+	  elem++;
+ }
+ elem=temp;
+  while(*elem!='\0')
+	   {
+		     printf("%3c",*elem);
+		       elem++;
+		        }
+   elem=temp;
+     while(*elem !='\0')
+	      {
+		        push(&s,*elem);
+			  elem++;
+			   }
+      //取出元素,调用pop函数
+         printf("\n出栈顺序：\n");
+	  //while(s.top!=s.base!=NULL)
+	  while(s.top!=NULL && s.base!=NULL)
+		   {
+			     printf("%3c",pop(&s));
+			      }
+	   
+	   clearStack(&s);
 
-#include "include/my_stack.h"
-
-int main(void) {
-	int x;	
-
-        while ( ! stack_is_full() ) {
-		scanf("%d", &x);
-		stack_push(x);
-	}
-	
-
-	while ( !stack_is_empty() ) {
-		x = stack_pop();
-
-		printf("%d\t", x);
-	}
-	printf("\n");
-
-	return 0;
+	    return 1;
 }
