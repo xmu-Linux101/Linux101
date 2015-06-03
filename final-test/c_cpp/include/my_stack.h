8aaa700bc7ab4b2c75718cec4a2f68eab3d9a04c
huihuizhang <83229659@qq.com>
2015-06-03 19:32:44 +0800

    Update my_stack.h

--- final-test/c_cpp/include/my_stack.h
+++ final-test/c_cpp/include/my_stack.h
@@ -1,24 +1,33 @@
 #ifndef _MY_STACK_H
 #define _MY_STACK_H
 
+typedef int Item;
+typedef struct node * PNode;
 
-void stack_push(int x);
-int stack_pop();
+typedef struct node
+{
+	Item data;
+	PNode down;
+}Node;
 
-// The capacity of the stack
-int stack_capacity();
+typedef struct stack
+{
+	PNode top;
+	int size;
+}Stack;
 
-// Current available size of the stack
-int stack_size();
+Stack *InitStack();
+
+int stack_is_empty(Stack *ps);
 
 
-/*
- * return 0/1 to check if stack is empty or full
- * 0 - No
- * 1 - Yes
- */
-int stack_is_empty();
-int stack_is_full();
+int stack_size(Stack *ps);
+int stack_capacity(Stack *ps);
 
+PNode GetTop(Stack *ps,Item *pitem);
+
+PNode stack_push(Stack *ps,Item item);
+
+PNode stack_pop(Stack *ps,Item *pitem);
 
 #endif //_MY_STACK_H
