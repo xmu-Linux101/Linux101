7605ea416d7e78f9f26ae81256a43b7393a11216
KeZekuan <715480954@qq.com>
2016-04-02 22:18:49 +0800

    请问不需要实现功能吗?

--- assignments/shell/AddUsers/addUsers.sh
+++ assignments/shell/AddUsers/addUsers.sh
@@ -1,9 +1,14 @@
-#
-#
+#!/usr/bin/env bash
 # useradd
 # chpasswd
 # chown
 # chmod
 # edquota
 #
-
+#~ case $1 in
+    #~ "add" ) echo 233;;
+    #~ "rm") 
+    #~ "chpwd")
+    #~ 
+    #~ *) echo "Unknown arguments.";;
+#~ esac
