fdb967714a9a537cc2deb0bdfc7547b23c14219e
zzc <240515044@qq.com>
2016-04-18 17:40:53 +0800

    This is a new file!

--- assignments/shell/AddUsers/addUsers.sh
+++ assignments/shell/AddUsers/addUsers.sh
@@ -1,9 +1,18 @@
+#!/bin/bash
 #
-#
-# useradd
-# chpasswd
-# chown
-# chmod
-# edquota
-#
-
+#batch add users with file called users.list
+# 
+for username in$(more users.list)
+do
+if [ -n $username ]
+then
+ useradd -m $username
+ echo
+ echo $username | passwd --stdin $username
+ echo
+ echo "User $username's password is changed!"
+else
+ echo "The username is null!"
+fi
+done
+~
