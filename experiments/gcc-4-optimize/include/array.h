913a4807823bce52d5a77eed175184702d2c281a
KeZekuan <715480954@qq.com>
2016-05-27 21:46:15 +0800

    233

--- experiments/gcc-4-optimize/include/array.h
+++
@@ -1,9 +0,0 @@
-#ifndef _ARRAY_H
-#define _ARRAY_H
-
-void array_fill_with(int *array, int length, int fillWith);
-void array_print(int *array, int length);
-
-void array_add(int *arrayX, int *arrayY, int *arrayZ, int length);
-
-#endif
