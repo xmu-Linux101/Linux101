c52de9c91bb2acb504643ada3d0ed600c7edba9d
FengXingyuan <landswimmer@hotmail.com>
2017-04-09 21:35:43 +0800

    Update addUsers.sh

--- assignments/shell/AddUsers/addUsers.sh
+++ assignments/shell/AddUsers/addUsers.sh
@@ -1,9 +1,10 @@
-#
-#
-# useradd
-# chpasswd
-# chown
-# chmod
-# edquota
-#
-
+#!/bin/bash
+#chmod 700 addUser.sh
+# ./addUser.sh
+file_list=`cat users.txt`
+for user in $file_list
+do
+	useradd $user
+	echo $user:12345 | chpasswd
+	echo "$user create success!"
+done
