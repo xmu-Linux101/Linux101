6708babf990543b92e6d28889e6f9d9d68d5fe22
Vancir <vancirprince@gmail.com>
2017-05-09 16:20:10 +0800

    完成了ubuntu下根据users.txt批量创建用户并写了delUsers.sh批量删除用户

--- assignments/shell/AddUsers/addUsers.sh
+++ assignments/shell/AddUsers/addUsers.sh
@@ -1,9 +1,22 @@
-#
-#
-# useradd
-# chpasswd
-# chown
-# chmod
-# edquota
-#
-
+#!/bin/bash
+# Script to add a user to Linux system
+for userlist in $(more ./users.txt)
+do
+if [ $(id -u) -eq 0 ]; then
+        username="$userlist"
+        password="$2"
+        echo "Creating $username"
+        egrep "^$username" /etc/passwd >/dev/null
+        if [ $? -eq 0 ]; then
+                echo "$username exists!"
+                exit 1
+        else
+                pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
+                useradd -G adm,dialout,cdrom,floppy,sudo,audio,dip,video,plugdev,netdev -m -p $pass $username
+                [ $? -eq 0 ] && echo "User $username has been added to system!" || echo "Failed to add a $username!"
+        fi
+else
+        echo "Only root may add a user to the system"
+        exit 2
+fi
+done
