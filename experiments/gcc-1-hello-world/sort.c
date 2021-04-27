d841e740481656e78a2fdbeb7c4bbb2a8af92e00
createing3 <58336402+createing3@users.noreply.github.com>
2021-04-27 20:25:18 +0800

    Update sort.c

--- experiments/gcc-1-hello-world/sort.c
+++ experiments/gcc-1-hello-world/sort.c
@@ -3,7 +3,7 @@
 void bubble_sort(int a[], int n) {
 	int i, j, temp;
 
-	for (i = 0; i < n - 1; j++) {
+	for (i = 0; i < n - 1; i++) {
 		for (j = 0; j < n - i - 1; j++) {
 			if (a[j] > a[j + 1]) {
 				temp = a[j];
